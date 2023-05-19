3235851112da31e8924b75657e3965b5d100790c
mizdra <pp.mizdra@gmail.com>
2023-05-19 10:47:42 +0900

    生の JavaScript で React を書く機会がほぼないため、常に react/prop-types を無効化するように

--- +react.js
+++ +react.js
@@ -1,14 +1,5 @@
 // @ts-check
 
-/** @type import('eslint').Linter.ConfigOverride */
-const configOverrideForTS = {
-  files: ['*.ts', '*.tsx'],
-  rules: {
-    // TypeScript では propTypes は使わず TypeScript の型注釈を使えば良いので off にする
-    'react/prop-types': 0,
-  },
-};
-
 /** @type import('eslint').Linter.BaseConfig */
 module.exports = {
   plugins: ['react', 'react-hooks'],
@@ -35,5 +26,4 @@ module.exports = {
     // コーディングスタイル統一のため、`<Component />` の形式で記述できる場合はそのように記述する
     'react/self-closing-comp': 2,
   },
-  overrides: [configOverrideForTS],
 };
