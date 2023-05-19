553e86c4d91973afd947147e3c147f0f121cbb3e
mizdra <pp.mizdra@gmail.com>
2023-05-19 10:48:34 +0900

    React 17 で追加された新しい JSX Transform を利用するための設定 (plugin:react/jsx-runtime) を追加

--- +react.js
+++ +react.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     // react
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime', // React 17 で追加された新しい JSX Transform を利用するための設定
     'plugin:react-hooks/recommended',
   ],
   parserOptions: {
