3a59530bee9d2294559fe02f528d8cee469e09a9
Susisu <susisu@hatena.ne.jp>
2024-10-07 10:51:05 +0900

    next: "strict" での設定ミスを修正

--- configtest/next/eslint.config.mjs
+++ configtest/next/eslint.config.mjs
@@ -1,7 +1,7 @@
 import config from '@hatena/eslint-config-hatena/flat';
 import globals from 'globals';
 
-export default config({ next: true }, [
+export default config({ next: 'strict' }, [
   {
     languageOptions: {
       globals: { ...globals.node },
