956dc720d586d252a65e443b3a67ea17e0df7682
Susisu <susisu@hatena.ne.jp>
2024-10-07 10:32:05 +0900

    型定義の更新漏れを修正

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,27 +4,33 @@ import { Config, ConfigWithExtends } from 'typescript-eslint';
 type ConfigOptions = Readonly<
   Partial<{
     /**
-     * TypeScript の設定ファイル (https://typescript-eslint.io/packages/parser#project)
+     * TypeScript の設定ファイル (https://typescript-eslint.io/packages/parser#project).
      * デフォルト: true (lint 対象のファイルに最も近い tsconfig.json を利用する)
      */
     tsProject: ParserOptions['project'];
     /**
-     * TypeScript のプロジェクトの設定 (https://typescript-eslint.io/packages/parser#projectservice)
+     * TypeScript のプロジェクトの設定 (https://typescript-eslint.io/packages/parser#projectservice).
      * デフォルト: false (無効)
      */
     tsProjectService: ParserOptions['projectService'];
     /**
-     * tsconfig.json の探索先のルートディレクトリ (https://typescript-eslint.io/packages/parser#tsconfigrootdir)
+     * tsconfig.json の探索先のルートディレクトリ (https://typescript-eslint.io/packages/parser#tsconfigrootdir).
      * デフォルト: undefined
      */
     tsconfigRootDir: ParserOptions['tsconfigRootDir'];
     /**
-     * React を使用するか. true の場合, React に関連する設定を有効にする
+     * React を使用するか. true の場合, React に関連する設定を有効にする.
      * デフォルト: false
      */
     react: boolean | undefined;
     /**
-     * Prettier を使用するか. true の場合, eslint-config-prettier を使ってフォーマットに関するルールを無効化する
+     * true の場合, React に関する設定と eslint-config-next 相当の設定を有効にする.
+     * `'strict'` を指定した場合, Core Web Vitals に関するルールを追加で有効にする.
+     * デフォルト: false
+     */
+    next: boolean | 'strict' | undefined;
+    /**
+     * Prettier を使用するか. true の場合, eslint-config-prettier を使ってフォーマットに関するルールを無効化する.
      * デフォルト: true
      */
     prettier: boolean | undefined;
